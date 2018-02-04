0364dc623bb648f140d405e0954718348649db19
Elizabeth <jukilove.c@gmail.com>
2018-02-04 13:15:23 -0800

    Add Required  User Stores

---
+++ flix_demo/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  flix_demo
+//
+//  Created by Elizabeth on 2/2/18.
+//  Copyright © 2018 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
