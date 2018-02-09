267da1b3310633e006ccd2d650bb341320a7b71f
Elizabeth <jukilove.c@gmail.com>
2018-02-09 12:02:41 -0800

    added collectionView

---
+++ flix_demo/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by Elizabeth on 2/9/18.
+//  Copyright © 2018 Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
